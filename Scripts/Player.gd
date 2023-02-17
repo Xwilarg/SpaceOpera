29d005b72c31d317b51d53e9a9061f9dc5c1c264
Xwilarg <xwilarg@protonmail.com>
2023-02-17 21:47:23 +0100

    Add 2D player

---
+++ Scripts/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var speed = 200
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var x = 0
+	var y = 0
+
+	if Input.is_action_pressed("move_right"):
+		x = 1
+	elif Input.is_action_pressed("move_left"):
+		x = -1
+	if Input.is_action_pressed("move_up"):
+		y = -1
+	elif Input.is_action_pressed("move_down"):
+		y = 1
+
+	var velocity = (global_transform.y * y + global_transform.x * x).normalized()
+	move_and_slide(velocity * speed)
