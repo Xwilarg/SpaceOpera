34812460b76da1e11b2280c596df809bcbcb515b
Xwilarg <xwilarg@protonmail.com>
2023-02-18 20:54:06 +0100

    Add new font

---
+++ Assets/Scripts/Computer/ComputerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SpaceOpera.Computer
+{
+    public class ComputerManager : MonoBehaviour
+    {
+        public static ComputerManager Instance { private set; get; }
+
+        [SerializeField]
+        private GameObject _computer;
+
+        [SerializeField]
+        private Transform _screenContainer;
+
+        public bool IsEnabled => _computer.activeInHierarchy;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void Show(ScreenType type)
+        {
+
+        }
+    }
+}
